4afdc809ad2ed0425429a8716754fac599c68888
Washington Siqueira <Washington Siqueira@DESKTOP-64LLE0P>
2023-05-30 15:54:35 -0300

    Adicionar arquivos de projeto.

---
+++ EMPOWERHELLOWORLD_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EMPOWERHELLOWORLD_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EMPOWERHELLOWORLD_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2427CA12-59BA-4CCC-911C-3ED1D99EB4D2")]
+[assembly: AssemblyVersion("1.0.0.0")]
